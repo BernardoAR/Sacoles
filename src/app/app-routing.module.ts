7df9e2dc935fa7be73d14231fbf9fcb9d078d1c0
BernardoAR <bernardo.roballo@aluno.iffar.edu.br>
2019-11-23 16:58:39 -0300

    Criado guard para sessão e restrições nas rotas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { SessionGuard } from './services/session.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'sacoles', loadChildren: './pages/private/sacoles/sacoles.module#SacolesPageModule' },
-  { path: 'tipo', loadChildren: './pages/private/tipo/tipo.module#TipoPageModule' },
-  { path: 'venda', loadChildren: './pages/private/venda/venda.module#VendaPageModule' },
-  { path: 'sabor', loadChildren: './pages/private/sabor/sabor.module#SaborPageModule' },
+  { path: 'sacoles', loadChildren: './pages/private/sacoles/sacoles.module#SacolesPageModule', canActivate: [SessionGuard] },
+  { path: 'tipo', loadChildren: './pages/private/tipo/tipo.module#TipoPageModule', canActivate: [SessionGuard] },
+  { path: 'venda', loadChildren: './pages/private/venda/venda.module#VendaPageModule', canActivate: [SessionGuard] },
+  { path: 'sabor', loadChildren: './pages/private/sabor/sabor.module#SaborPageModule', canActivate: [SessionGuard] },
   { path: 'login', loadChildren: './pages/public/login/login.module#LoginPageModule' },
   { path: 'cadastro', loadChildren: './pages/public/cadastro/cadastro.module#CadastroPageModule' },
   { path: 'recupera-senha', loadChildren: './pages/public/recupera-senha/recupera-senha.module#RecuperaSenhaPageModule' },
-  { path: 'sistema', loadChildren: './pages/private/sistema/sistema.module#SistemaPageModule' }
+  { path: 'sistema', loadChildren: './pages/private/sistema/sistema.module#SistemaPageModule', canActivate: [SessionGuard] }
 ];
 
 @NgModule({
