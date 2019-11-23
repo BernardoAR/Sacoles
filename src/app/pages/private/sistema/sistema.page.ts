ceb3b5e1c73459f5d38b5a453cc5be643ba3376a
BernardoAR <bernardo.roballo@aluno.iffar.edu.br>
2019-11-23 18:12:34 -0300

    Criação de método de logout, mostragem do menu correspondente.

--- src/app/pages/private/sistema/sistema.page.ts
+++ src/app/pages/private/sistema/sistema.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-sistema',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sistema.page.scss'],
 })
 export class SistemaPage implements OnInit {
+  usuarioLogado;
 
-  constructor() { }
+  constructor(private login: LoginService, private menu: MenuController) {
+    this.usuarioLogado = this.login.usuarioLogado;
+    this.menu.enable(true);
+  }
 
   ngOnInit() {
   }
-
+  public sair() {
+    this.login.logout();
+  }
 }
