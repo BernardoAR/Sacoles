6630bc99ca2cd8fb9d69c154e8d7d678a6b4f35d
BernardoAR <bernardo.roballo@aluno.iffar.edu.br>
2019-11-23 15:58:41 -0300

    Configuração básica para a conexão com o Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAp8_CvT1jcZiuAkIYLE7r6RlW9L44gZGs",
+    authDomain: "sacoles-ec416.firebaseapp.com",
+    databaseURL: "https://sacoles-ec416.firebaseio.com",
+    projectId: "sacoles-ec416",
+    storageBucket: "sacoles-ec416.appspot.com",
+    messagingSenderId: "880767800150",
+    appId: "1:880767800150:web:2a855ad5a3d24b75a87cf7",
+    measurementId: "G-K8RZYMCYWL"
+  }
 };
 
 /*
