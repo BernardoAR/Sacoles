f7fe3e1fd714311f7dd8e3b96b508bde7749d23b
BernardoAR <bernardo.roballo@aluno.iffar.edu.br>
2019-11-25 12:46:40 -0300

    Criação do sistema para a listagem da api e dos sacolés

--- src/app/pages/private/sistema/sistema.page.ts
+++ src/app/pages/private/sistema/sistema.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/services/login.service';
 import { MenuController } from '@ionic/angular';
 import { WeatherService } from 'src/app/services/weather.service';
+import { FirestoreService } from 'src/app/services/firestore.service';
 
 @Component({
   selector: 'app-sistema',
@@ -12,17 +13,19 @@ export class SistemaPage implements OnInit {
   usuarioLogado;
   resposta: any;
   icone: string;
+  sacoles: any;
   constructor(
     private login: LoginService,
     private menu: MenuController,
-    private temp: WeatherService
+    private temp: WeatherService,
+    private fs: FirestoreService
   ) {
     this.temp.temperatura('São Borja').subscribe(resp => {
       this.resposta = resp;
       const codigo = resp.weather[0].icon;
       this.icone = `http://openweathermap.org/img/wn/${codigo}@2x.png`;
     });
-    console.log();
+    this.sacoles = fs.listarOrd('sacole', 'quantidade', 'asc');
     this.usuarioLogado = this.login.usuarioLogado;
     this.menu.enable(true);
   }
